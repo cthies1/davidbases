0ab669a8d6f97d929e3b4bb671aa575c77850412
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-13 14:46:24 -0700

    inputForm

---
+++ inputPassenger.php
@@ -0,0 +1,20 @@
+<?php
+    try {
+        //open the sqlite database file
+        $db = new PDO('sqlite:./myDB/airport.db');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //insert the passenger (UNSAFE!)
+        //order matters (look at your schema)
+        $stmt = "INSERT INTO passengers VALUES
+            ('$_POST[firstName]', '$_POST[lastNname]', '$_POST[ssn]');";
+        $db->exec($stmt);
+        //disconnect from database
+        $db = null;
+    }
+    catch(PDOException $e)
+    {
+    die('Exception : '.$e->getMessage());
+    }
+    //redirect user to another page
+    header("Location: showPassengers.php");
+?>
