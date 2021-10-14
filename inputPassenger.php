5a64b3dcc1e27990514910815968fc44a5e87588
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-13 20:26:03 -0700

    Update inputPassenger.php

--- inputPassenger.php
+++ inputPassenger.php
@@ -1,13 +1,22 @@
 <?php
     try {
+        if(null !== ('$_POST[f_name]')){
+        var_dump('$_POST[f_name]');
+        //header("Location: inputForm.html"); 
+        }
         //open the sqlite database file
         $db = new PDO('sqlite:./myDB/airport.db');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //insert the passenger (UNSAFE!)
         //order matters (look at your schema)
-        $stmt = "INSERT INTO passengers VALUES
-            ('$_POST[f_name]', '$_POST[m_name]', '$_POST[l_name]', '$_POST[ssn]');";
-        $db->exec($stmt);
+        $stmt =$db->prepare("INSERT INTO passengers VALUES
+            (:f_name, :m_name, :l_name, :ssn);");
+        $stmt->bindValue(':f_name','$POST_[f_name]');
+        $stmt->bindValue(':m_name','$POST_[m_name]');
+        $stmt->bindValue(':l_name','$POST_[l_name]');
+        $stmt->bindValue(':ssn','$POST_[ssn]');
+        $result = $stmt->execute();
+        //$db->exec($stmt);
         //disconnect from database
         $db = null;
     }
