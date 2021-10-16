e5e60a1145193c078db6004e99e5362a8144b303
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-15 18:09:32 -0700

    Update inputPassenger.php

--- inputPassenger.php
+++ inputPassenger.php
@@ -12,7 +12,6 @@
             $lNull = false;
         } 
         
-        //if(null != ($_POST['ssn']) and preg_match("/[0-9]{3}+-+[0-9]{2}+-+[0-9]{4}/", $_POST['ssn']) == 1){
         preg_match('/^[0-9]{3}-[0-9]{2}-[0-9]{4}$/', $_POST['ssn'], $matches);
         if(null != ($_POST['ssn']) and count($matches) > 0){
             $sNull = false;
