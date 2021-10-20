6bb9300fc1252e2629f98e737c781522663dfc14
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-20 14:45:17 -0700

    Update inputPassenger.php

--- inputPassenger.php
+++ inputPassenger.php
@@ -37,23 +37,25 @@
         }
         */
         if($fNull or $lNull or $sNull) {
+            echo "something went wrong!";
             header("Location: inputForm.html?error=0");
         }
+        else {
         
-        
-        //open the sqlite database file
-        $db = new PDO('sqlite:./myDB/airport.db');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //insert the passenger (UNSAFE!)
-        //order matters (look at your schema)
-        $stmt =$db->prepare("INSERT INTO passengers VALUES
-            (:f_name, :m_name, :l_name, :ssn);");
-        $stmt->bindValue(':f_name',$_POST['f_name']);
-        $stmt->bindValue(':m_name',$_POST['m_name']);
-        $stmt->bindValue(':l_name',$_POST['l_name']);
-        $stmt->bindValue(':ssn',$_POST['ssn']);
-        $result = $stmt->execute();
-        //$db->exec($stmt);
+            //open the sqlite database file
+            $db = new PDO('sqlite:./myDB/airport.db');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //insert the passenger (UNSAFE!)
+            //order matters (look at your schema)
+            $stmt =$db->prepare("INSERT INTO passengers VALUES
+                (:f_name, :m_name, :l_name, :ssn);");
+            $stmt->bindValue(':f_name',$_POST['f_name']);
+            $stmt->bindValue(':m_name',$_POST['m_name']);
+            $stmt->bindValue(':l_name',$_POST['l_name']);
+            $stmt->bindValue(':ssn',$_POST['ssn']);
+            $result = $stmt->execute();
+            //$db->exec($stmt);
+        }
         //disconnect from database
         $db = null;
     }
