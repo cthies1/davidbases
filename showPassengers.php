21d11714abbea0e84881eb20717f5ab427713ae2
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-22 15:28:26 -0700

    Update showPassengers.php

--- showPassengers.php
+++ showPassengers.php
@@ -25,12 +25,12 @@
             $query_str = "select * from passengers;";
             $result_set = $db->query($query_str);
 
-            echo "<table>";
-            echo "<tr>";
+            echo "<table style='border:1px solid black; border-collapse: collapse;'>";
+            echo "<tr style='border:1px solid black; border-collapse: collapse;'>";
                 echo "<th>SSN</th><th>First Name</th><th>Middle Name</th><th>Last Name</th>";
             echo "</tr>";
             foreach($result_set as $tuple) {
-                echo "<tr>";
+                echo "<tr style='border:1px solid black; border-collapse: collapse;'>";
                 echo "<td>$tuple[ssn]</td>";
                 echo "<td>$tuple[f_name]</td>";
                 echo "<td>$tuple[m_name]</td>";
