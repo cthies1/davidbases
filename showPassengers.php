2d65a4abbc90f8f40d5f4b51e9a3191856569148
Chloe Thies <chloe.r.thies@gmail.com>
2021-10-22 15:26:47 -0700

    Update showPassengers.php

--- showPassengers.php
+++ showPassengers.php
@@ -1,19 +1,3 @@
-<?php
-/*
-    $db_file = './myDB/airport.db';
-    try {
-        //open connection to the airport database file
-        $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
-        //set errormode to use exceptions
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e) {
-        die('Exception : '.$e->getMessage());
-    }
-    */
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -32,26 +16,20 @@
 
         try {
             //open connection to the airport database file
-            $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
+            $db = new PDO('sqlite:' . $db_file);
 
             //set errormode to use exceptions
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             //return all passengers, and store the result set
-            $query_str = "select * from passengers;";  // <----- Line 19
+            $query_str = "select * from passengers;";
             $result_set = $db->query($query_str);
 
-            //loop through each tuple in result set and print out the data
-            //ssn will be shown in blue (see below)
-       
-            //foreach($result_set as $tuple) {          // <------ Line 24
-             //   echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name]<br/>\n";
-            //}
             echo "<table>";
             echo "<tr>";
                 echo "<th>SSN</th><th>First Name</th><th>Middle Name</th><th>Last Name</th>";
             echo "</tr>";
-            foreach($result_set as $tuple) {          // <------ Line 24
+            foreach($result_set as $tuple) {
                 echo "<tr>";
                 echo "<td>$tuple[ssn]</td>";
                 echo "<td>$tuple[f_name]</td>";
@@ -70,27 +48,6 @@
             die('Exception : '.$e->getMessage());
         }
         ?>
-
-        <!-- <table>
-            <tr>
-                <th>SSN</th>
-                <th>First Name</th>
-                <th>Middle Name</th>
-                <th>Last Name</th>
-            </tr>
-            
-            <tr>
-             foreach($result_set as $tuple) {          // <------ Line 24
-                echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name]<br/>\n";
-                echo "<td>$tuple[ssn]</td>"
-                echo "<td>$tuple[f_name]</td>"
-                echo "<td>$tuple[m_name]</td>"
-                echo "<td> $tuple[l_name]</td>"
-                echo "<td><a href="">Update</a></td>"
-             }    
-            </tr>
-            
-        </table> -->
         
     </body>
 </html>
